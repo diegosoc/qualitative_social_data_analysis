6604955e4b21cebef1c3fcad297b2fab2c35dd53
Diego Dorrego <96392293+diegosoc@users.noreply.github.com>
2023-12-27 16:32:05 +0000

    Update terms_vis.py

--- src/terms_vis.py
+++ src/terms_vis.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
+import plotly.io as pio
 import nltk
 from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.corpus import stopwords
@@ -8,7 +9,7 @@ nltk.download('stopwords')
 
 # One of the most important information that we can get from some documents is the key terms.
 # Using TF-IDF metrics we can see which terms are most important.
-# It can be usefull for social scientist. We can se, for the informers, the most important termins used by each one:
+# It can be usefull for social scientist. We can see, for all the documents and for each informers, the most important termins used by each one:
 
 stop_words_espanol = set(stopwords.words('spanish'))
 
@@ -20,10 +21,57 @@ stopwords_add = ['tal', 'vez', 'todo', 'toda', 'todos', 'todas', 'tal', 'si', 'n
                  'seguimos', 'da', 'de', 'dé', 'aquel', 'tan', 'mas', 'más']
 
 stop_words_espanol.update(stopwords_add)
-
 # Transform set into list:
 stop_words_esp = list(stop_words_espanol)
 
+# Create the TD-IDF model and fit for ALL documents:
+tfidf_vectorizer = TfidfVectorizer(stop_words=stop_words_esp)
+tfidf_matrix = tfidf_vectorizer.fit_transform(df['text_cleaned'])
+feature_names = tfidf_vectorizer.get_feature_names_out()
+av_metrics = np.mean(tfidf_matrix, axis=0).tolist()[0]
+metrics = list(zip(feature_names, av_metrics))
+sorted_metrics = sorted(metrics, key=lambda x: x[1], reverse=True)
+
+# Show first 10 most important terms in all the documents:
+for term, metric in sorted_metrics[:10]:
+    print(f'{term}: {metric}')
+  
+# Visualization TF-IDF information:
+# Create a dictionary with the sorted terms and their TF-IDF metrics of all documents:
+metrics_dict = dict(sorted(sorted_metrics[:7], key=lambda x: x[1], reverse=True))
+terms = [f"{metrics_dict[termino]:.3f}" for term in metrics_dict]
+
+# Create barchart for TF-IDF metrics for all documents
+fig = go.Figure()
+fig.add_trace(go.Bar(
+    x=list(metrics_dict.values()),
+    y=list(metrics_dict.keys()),
+    orientation='h',
+    text=etiquetas,
+    textposition='outside',
+    marker=dict(color='purple')
+))
+fig.update_layout(
+    title={
+        'text': 'Términos más importantes utilizados por usuarios de izquierdas',
+        'font': {'size': 18, 'family': 'Arial', 'color': 'black'},
+        'x': 0.5,
+        'y': 0.9
+    },
+    xaxis_title='Puntuación media de TF-IDF',
+    yaxis_title='Términos',
+    xaxis_range=[0, 0.20],
+    yaxis={
+        'autorange': 'reversed',
+        'tickfont': {'size': 16},
+    },
+    font={
+        'size': 14,
+    }
+)
+fig.show()
+
+# Create a function to get TF-IDF information of each informer:
 def tfidf_informer(df: pd.DataFrame) -> bar charts:
     # Extract the informers index:
     informers = df['informer'].unique()
@@ -38,8 +86,8 @@ def tfidf_informer(df: pd.DataFrame) -> bar charts:
         metrics = list(zip(feature_names, av_tfidf_metrics))
 
         # TF-IDF average sorted:
-        sorted_metrics = sorted(puntuaciones, key=lambda x: x[1], reverse=True)
-        metrics_dict = dict(sorted(puntuaciones_ordenadas[:7], key=lambda x: x[1], reverse=True))
+        sorted_metrics = sorted(metrics, key=lambda x: x[1], reverse=True)
+        metrics_dict = dict(sorted(sorted_metrics[:7], key=lambda x: x[1], reverse=True))
         terms = [f"{metrics_dict[term]:.3f}" for term in metrics_dict]
 
         # Create charts for each informer:
