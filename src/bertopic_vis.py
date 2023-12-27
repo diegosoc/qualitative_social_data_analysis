8e3969318a3cbe56185232aff56be986d48f143c
Diego Dorrego <96392293+diegosoc@users.noreply.github.com>
2023-12-27 15:20:37 +0000

    Update bertopic_vis.py

--- src/bertopic_vis.py
+++ src/bertopic_vis.py
@@ -11,11 +11,15 @@ topics, ini_probs= bert_model.fit_transform(df['text_cleaned'])
 #Visualization of bertopic results:
 #1. Barchart with topics and probabilities:
 bert_model.visualize_barchart(top_n_topics = 16, n_words = 10)
+
 #2. Intertopic distance map:
 bert_model.visualize_topics()
+
 #3. Visualization of the topic-clusters:
 bert_model.visualize_documents(df['text_cleaned'])
+
 #4. Hierarchical clustering of topics:
 bert_model.visualize_hierarchy()
+
 #5. Similarity information (matrix) of the topics:
 bert_model.visualize_heatmap()
