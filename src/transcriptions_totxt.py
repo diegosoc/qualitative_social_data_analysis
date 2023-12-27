e97d320520e34c0c4bca93089e2d16dc55b5e902
Diego Dorrego <96392293+diegosoc@users.noreply.github.com>
2023-12-27 16:37:47 +0000

    Update transcriptions_totxt.py

--- src/transcriptions_totxt.py
+++ src/transcriptions_totxt.py
@@ -2,14 +2,14 @@ import os
 from pdfminer.high_level import extract_text
 import re
 
-def pdf_to_txt(transcriptions, transcriptionstxt):
-    if not os.path.exists(transcriptionstxt):
-        os.makedirs(transcriptionstxt)
-    file_pdf = [file for file in os.listdir(transcriptions) if file.lower().endswith('.pdf')]
+def pdf_to_txt(folder_transcriptions: str, folder_transcriptions_txt: str) -> Processed files:
+    if not os.path.exists(folder_transcriptions_txt):
+        os.makedirs(folder_transcriptions_txt)
+    file_pdf = [file for file in os.listdir(folder_transcriptions) if file.lower().endswith('.pdf')]
 
     for file_pdf in file_pdf:
-        path_pdf = os.path.join(transcriptions, file_pdf)
-        path_txt = os.path.join(transcriptionstxt, os.path.splitext(file_pdf)[0] + '.txt')
+        path_pdf = os.path.join(folder_transcriptions, file_pdf)
+        path_txt = os.path.join(folder_transcriptions_txt, os.path.splitext(file_pdf)[0] + '.txt')
         text = extract_text(path_pdf)
 
         with open(path_txt, "w", encoding='utf-8') as file_txt:
