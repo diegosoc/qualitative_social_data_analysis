40f32ef15c90c3fcd2a6c310b3028aded7778c01
Diego Dorrego <96392293+diegosoc@users.noreply.github.com>
2023-12-27 17:11:03 +0000

    Update unzip_function.py

--- src/unzip_function.py
+++ src/unzip_function.py
@@ -1,32 +1,27 @@
 import os
 import zipfile
 
-#folder : You have to provide folder with zip file is
-#study_lst : You have to provide a list of strings with the studies you are interested in (list = ['325','234'])
-#unziped_files_folder : You have to provide folder where the files will be unzipped or create new one
+#It is necessary to create a function to unzip the CIS folder:
 
-import os
-import zipfile
-
-def unzip_data(folder, studies_list, unziped_files_folder):
-    if not os.path.exists(folder):
-        print(f'The folder named {folder} does not exist')
+def unzip_data(studies_folder: str, studies_list: list, unziped_files_folder: str) -> folder:
+    if not os.path.exists(studies_folder):
+        print(f'The folder named {studies_folder} does not exist')
         return
     if not os.path.exists(unziped_files_folder):
         os.makedirs(unziped_files_folder)
-    files = os.listdir(folder)
-    file_paths = []
-    files_to_unzip = []
-    for file in files:
-        file_path = os.path.join(folder, file)
-        file_paths.append(file_path)
-    for file_path in file_paths:
-        if file_path.endswith('.zip'):
-            files_to_unzip.append(file_path)
-    for file in files_to_unzip:
-        if any(str(study) in file for study in studies_list):
-            with zipfile.ZipFile(file, 'r') as zip_ref:
+    folders = os.listdir(studies_folder)
+    folder_paths = []
+    folders_to_unzip = []
+    for folder in folders:
+        folder_path = os.path.join(studies_folder, folder)
+        folder_paths.append(folder_path)
+    for folder_path in folder_paths:
+        if folder_path.endswith('.zip'):
+            folders_to_unzip.append(folder_path)
+    for folder in folders_to_unzip:
+        if any(str(study) in folder for study in studies_list):
+            with zipfile.ZipFile(folder, 'r') as zip_ref:
                 zip_ref.extractall(unziped_files_folder)
-            print(f"Se ha descomprimido {file} en {unziped_files_folder}")
+            print(f"{folder} has been unzipped in {unziped_files_folder}")
 
 unzip_data('qualitative_zipped_data', [3251], 'unziped_data')
