f1735dc2c206f4f628e2213ddaa2f56ea9972346
Diego Dorrego <96392293+diegosoc@users.noreply.github.com>
2023-12-27 15:31:11 +0000

    Update edit_txt_files.py

--- src/edit_txt_files.py
+++ src/edit_txt_files.py
@@ -1,23 +1,26 @@
 import re
 import os 
 
-def procesar_archivos_en_carpeta(carpeta):
-    # Obtener la lista de archivos en la carpeta
-    archivos = [f for f in os.listdir(carpeta) if f.endswith('.txt')]
+#It is necessary to create a function to process the text files in order to get ready to transform in a Dataframe and work with it:
+#This step depends on the style of the CIS documents, I took the example of the most recent studies, but it could be different:
 
-    # Iterar sobre cada archivo
-    for archivo in archivos:
-        filename = os.path.join(carpeta, archivo)
+def process_file_in_folder (folder: str):
+    
+    # Get a list with the files in folder:
+    files = [f for f in os.listdir(folder) if f.endswith('.txt')]
 
-        # Read the content of the file
+    # Get the full filenames with the path:
+    for file in files:
+        filename = os.path.join(folder, file)
 
+        # Read the content of each file:
         with open(filename, 'r', encoding = 'utf-8') as file:
             content = file.read()
 
-        # Split the content into paragraphs
+        # Split the content into paragraphs using regular expresions:
         paragraphs = re.split(r'(\n\s*\n)', content)
 
-        # Process each paragraph
+        #Process each paragraph:
         for i in range(0, len(paragraphs), 2):
             paragraph = paragraphs[i]
             if not any(c.islower() for c in paragraph):
@@ -26,7 +29,7 @@ def procesar_archivos_en_carpeta(carpeta):
         # Join the modified paragraphs back together
         modified_content = ''.join(paragraphs)
 
-        # Write the modified content back to the file
+        #Write the modified content back to the file
         with open(filename, 'w', encoding = 'utf-8') as file:
             file.write(modified_content)
 
