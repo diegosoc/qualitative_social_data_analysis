a64529e67659f3daba6f72977ec43d715e7cbeae
Diego Dorrego <96392293+diegosoc@users.noreply.github.com>
2023-12-27 22:09:59 +0000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
-#APPLYING THE CODE TO THE STUDIE 36751:
-from unzip_function import unzip_data
+# APPLYING THE CODE TO THE STUDY 3251:
+# STUDY TITLE: "ESTUDIO CUALITATIVO SOBRE EL PROCÉS DE INDEPENDENCIA EN CATALUÑA (2018)
+# "QUALITATIVE STUDY ON INDEPENDENCE OF CATALONIA (2018)"
 
+from unzip_function import unzip_data
 unzip_data('qualitative_zipped_data', 3251, 'qualitative_unzipeed_data')
